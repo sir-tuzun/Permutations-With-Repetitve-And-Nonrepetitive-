94ef9f858e86d62a1b56f4faf598253f343280b8
tuzun <103971513+sir-tuzun@users.noreply.github.com>
2023-03-11 05:57:56 +0300

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -11,8 +11,7 @@ public class Permutation {
         String permutation = input.nextLine();
         long start;
         long finish; 
-        
-        
+
         // Need a counter which is stored in heap.
         int[] count = {0};          
         
@@ -20,10 +19,8 @@ public class Permutation {
         boolean b1 = isRepetitive(permutation);
         
         /* 
-        if string is repetitive the program will execute override "loops" method
+        if string is repetitive the program will execute override "loops" method.
         we need to know which letters is exist more than one at after the switch.
-        if there is no repetitive letter the porgram will not lost any time to declare 
-        the letters.
         */
         if (b1) {
             start = System.currentTimeMillis();
@@ -51,14 +48,13 @@ public class Permutation {
         
         markRepetitive = findRepetitive(s2);
         for (int i = 0; i < s2.length(); i++){
-            /* 
-            If on hand letter is repetitive program will not go into the this permutation 
-            because the letter's permutations are already occourded. It is going to analyze next letter.
-            */
+             
+            //If on hand letter is repetitive program will not go into the this permutation 
             if(markRepetitive[i])
                 continue;
             // Calling recursive method to create nested loops. The loops give us to analyze each step of string.
-            loops(changeLeft(s1, s2, i),changeRight(s2,i),count,markRepetitive);
+            else 
+                loops(changeLeft(s1, s2, i),changeRight(s2,i),count,markRepetitive);
         }
     }
     
@@ -72,20 +68,9 @@ public class Permutation {
             loops(changeLeft(s1, s2, i),changeRight(s2,i),count);
     }
     
-    private static String changeLeft(String s1, String s2, int i) 
-        {return s1 + s2.charAt(i);}
-
-    private static String changeRight(String s2, int i) 
-        {return s2.substring(0, i)+ s2.substring(i+1);}
-    
     private static boolean isRepetitive(String permutation) {
-        
         for (int i = 0;i < permutation.length(); i++) {
-            for (int j = i+1; j < permutation.length(); j++){
-                /* 
-                If there is finded a repetitive letter the program 
-                will return true to exit from method and do not lose time.
-                */    
+            for (int j = i+1; j < permutation.length(); j++){  
                 if (permutation.charAt(i) == permutation.charAt(j))
                     return true;
             }
@@ -106,4 +91,10 @@ public class Permutation {
         }
         return markRepetitive;
     }
+    
+    private static String changeLeft(String s1, String s2, int i) 
+        {return s1 + s2.charAt(i);}
+
+    private static String changeRight(String s2, int i) 
+        {return s2.substring(0, i)+ s2.substring(i+1);}
 }
